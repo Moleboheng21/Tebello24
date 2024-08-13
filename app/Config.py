69dc16573e4f606b70e4afa7da0260dd48649e96
Moleboheng21 <112688353+Moleboheng21@users.noreply.github.com>
2024-08-13 08:59:19 +0200

    atlas

--- app/Config.py
+++ app/Config.py
@@ -1,4 +1,4 @@
 class Config:
-    MONGO_URI = "mongodb://localhost:27017/Signup"
+    MONGO_URI = "mongodb+srv://moleboheng023:kcFXn6przQ0Z3MVf@cluster0.sgk49cz.mongodb.net/mydatabase"
     JWT_TOKEN_LOCATION = 'headers'
     SECRET_KEY = 'xcvbn'
